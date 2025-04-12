b83f19667effbdd3fde616ddb3056793f59f41b0
RafaGomezGuillen <rafagomezguillen03@gmail.com>
2025-04-12 11:28:08 +0100

    style: Refactor SkillsetCard component and CSS for improved layout and subtitle support

--- src/components/Cards/SkillsetCard/SkillsetCard.jsx
+++ src/components/Cards/SkillsetCard/SkillsetCard.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./SkillsetCard.css";
 
-const SkillsetCard = ({ icon: Icon, link, title }) => {
+const SkillsetCard = ({ icon: Icon, link, title, subtitle }) => {
   return (
-    <a href={link} title={title} target="_blank" rel="noreferrer">
+    <a href={link} title={title} target="_blank" rel="noreferrer" className="skillset-card-container">
       <div className="skillset-card">
         <Icon></Icon>
+      </div>
+
+      <div className="skillset-card-title">
+        <p>{subtitle}</p>
       </div>
     </a>
   );
